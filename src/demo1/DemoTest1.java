4e2d8134f4226c2ba9297b3cae7277ebd8440b28
Argue <Argue@LAPTOP-O82B69D3>
2025-01-29 20:11:23 -0600

    Integrated add method

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -3,5 +3,13 @@ package demo1;
 public class DemoTest1 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+	
+	int a= 10;
+	int b= 20;
+	
+	int result = a + b;
+		
+	System.out.println(a + b); 
 	}
 }
+
